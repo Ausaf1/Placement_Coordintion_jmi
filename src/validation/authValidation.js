19c195f432b09452d48e48b4f5b53d3c78350a07
Ausaf1 <ausaf.esrar@gmail.com>
2022-03-03 20:21:33 +0530

    auth register user

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -1,25 +1,27 @@
 import { check } from "express-validator";
 
 let validateRegister = [
-    check("email", "Invalid email").isEmail().trim(),
+  check("email", "Invalid email").isEmail().trim(),
 
-    check("password", "Invalid password. Password must be at least 2 chars long")
-    .isLength({ min: 2 }),
+  check(
+    "password",
+    "Invalid password. Password must be at least 6 characters long."
+  ).isLength({ min: 6 }),
 
-    check("passwordConfirmation", "Password confirmation does not match password")
-    .custom((value, { req }) => {
-        return value === req.body.password
-    })
+  check("passwordConfirmation", "Passwords do not match.").custom(
+    (value, { req }) => {
+      return value === req.body.password;
+    }
+  ),
 ];
 
 let validateLogin = [
-    check("email", "Invalid email").isEmail().trim(),
+  check("email", "Invalid email").isEmail().trim(),
 
-    check("password", "Invalid password")
-    .not().isEmpty()
+  check("password", "Invalid password").not().isEmpty(),
 ];
 
 module.exports = {
-    validateRegister: validateRegister,
-    validateLogin: validateLogin
+  validateRegister: validateRegister,
+  validateLogin: validateLogin,
 };
