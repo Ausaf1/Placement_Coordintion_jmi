f7dec52b20acb3300a0d6d3d6bc95be59ca71aeb
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-04-28 01:48:45 +0530

    refactored code and handlee responsiveness along with navbar-img

--- src/public/js/landingPage.js
+++ src/public/js/landingPage.js
@@ -46,3 +46,10 @@ $('#button-a').click(function() {
         })
     }
 });
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+});
